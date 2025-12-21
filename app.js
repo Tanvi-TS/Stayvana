ca96be4bd63ef44f2aeb9192e9ded1c653ba3214
Tanvi-TS <tanvisaxena412@gmail.com>
2025-12-22 00:52:07 +0530

    Apply listing validation

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 main().then(()=>{
     console.log("Connected to database");
@@ -26,6 +28,15 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const validateListing = (req, res, next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        throw new ExpressError(400, error.message);
+    }else{
+        next();
+    }
+}
+
 //index route 
 app.get("/listings", wrapAsync(async (req,res)=>{
     const allListings = await Listing.find({});
@@ -37,7 +48,8 @@ app.get("/listings/new", (req, res)=>{
     res.render("./listings/new.ejs");
 })
 
-app.post("/listings", wrapAsync(async (req, res)=>{
+app.post("/listings", validateListing,
+    wrapAsync(async (req, res)=>{
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings"); 
@@ -51,15 +63,17 @@ app.get("/listings/:id", wrapAsync(async (req, res)=>{
 }))
 
 //edit route
-app.get("/listings/:id/edit", wrapAsync(async (req, res)=>{
+app.get("/listings/:id/edit",wrapAsync(async (req, res)=>{
     let{id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/edit.ejs", {listing});
 }))
-app.put("/listings/:id", wrapAsync(async (req, res)=>{
+app.put("/listings/:id",validateListing,
+    wrapAsync(async (req, res)=>{
+
     let{id} = req.params;
     await Listing.findByIdAndUpdate(id, req.body.listing);
-    res.redirect("/listings");
+    res.redirect("/listings/"+id);
 }))
 
 //delete
@@ -74,9 +88,14 @@ app.get("/", (req,res)=>{
     res.send("Jai Ambey Maiya ki Jai");
 })
 
+//for all undefined paths 
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+});
+
 app.use((err, req, res, next) => {
-    console.error(err.message);
-    res.status(500).send(err.message);
+    let{statusCode="500", message="Something went wrong !"} = err;
+    res.render("error.ejs", {statusCode, message});
 });
 
 app.listen(port, ()=>{
