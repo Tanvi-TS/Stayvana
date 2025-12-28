a9112a15d52413d8d871ecc22c72499c50bde39a
Tanvi-TS <tanvisaxena412@gmail.com>
2025-12-28 14:07:24 +0530

    Re-style Rating

--- routes/listing.js
+++ routes/listing.js
@@ -4,34 +4,30 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 
-//index route 
-router.get("/", wrapAsync(listingController.index))
+router
+.route("/")
+.get(wrapAsync(listingController.index))
+.post(isLoggedIn, validateListing,
+    wrapAsync(listingController.createListing))
+
+router
+.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put(isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing))
+.delete(isLoggedIn, 
+    isOwner,
+    wrapAsync(listingController.deleteListing))
 
 //new listing
 router.get("/new", isLoggedIn, listingController.renderNewForm)
-
-router.post("/", isLoggedIn, validateListing,
-    wrapAsync(listingController.createListing))
-
-//show route
-router.get("/:id", wrapAsync(listingController.showListing))
 
 //edit route
 router.get("/:id/edit", 
     isLoggedIn,
     isOwner,
     wrapAsync(listingController.renderEditForm))
-
-router.put("/:id",
-    isLoggedIn,
-    isOwner,
-    validateListing,
-    wrapAsync(listingController.updateListing))
-
-//delete listing
-router.delete("/:id",
-    isLoggedIn, 
-    isOwner,
-    wrapAsync(listingController.deleteListing))
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const User = require("../models/user.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/user.js");
 
-// Signup
-router.get("/signup", userController.rendersignupForm)
+router
+.route("/signup")
+.get(userController.rendersignupForm)
+.post(wrapAsync(userController.signup))
 
-router.post("/signup", wrapAsync(userController.signup))
-
-//login 
-router.get("/login", userController.renderLoginForm)
-
-router.post("/login",
-    saveRedirectUrl,
+router
+.route("/login")
+.get(userController.renderLoginForm)
+.post(saveRedirectUrl,
     passport.authenticate('local', { 
         failureRedirect: '/login', 
         failureFlash: true
     }),
     userController.login)
 
-//logout
 router.get("/logout", userController.logout)
 
 module.exports= router;
