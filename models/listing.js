0d0e228b2bf03690ece908a87e628ce85353bccf
Tanvi-TS <tanvisaxena412@gmail.com>
2025-12-29 02:29:17 +0530

    Add image upload feature

--- models/listing.js
+++ models/listing.js
@@ -4,11 +4,10 @@ const listingSchema = new mongoose.Schema({
     title: String,
     description: String,
     image: {
-        type: String,
-        default: "https://plus.unsplash.com/premium_photo-1664303991463-36449a65d3d6?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set : (v)=> v=== ""? "https://plus.unsplash.com/premium_photo-1664303991463-36449a65d3d6?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
+        url: String,
+        filename: String,
     },
-    price: String,
+    price: Number,
     location: String,
     country: String,
     
--- routes/listing.js
+++ routes/listing.js
@@ -3,26 +3,34 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
 
 router
 .route("/")
 .get(wrapAsync(listingController.index))
-.post(isLoggedIn, validateListing,
+.post(
+    isLoggedIn, 
+    upload.single("listing[image]"),
+    validateListing,
     wrapAsync(listingController.createListing))
+
+//new listing
+router.get("/new", isLoggedIn, listingController.renderNewForm)
 
 router
 .route("/:id")
 .get(wrapAsync(listingController.showListing))
-.put(isLoggedIn,
+.put(
+    isLoggedIn,
     isOwner,
+    upload.single("listing[image]"),
     validateListing,
     wrapAsync(listingController.updateListing))
 .delete(isLoggedIn, 
     isOwner,
     wrapAsync(listingController.deleteListing))
-
-//new listing
-router.get("/new", isLoggedIn, listingController.renderNewForm)
 
 //edit route
 router.get("/:id/edit", 
